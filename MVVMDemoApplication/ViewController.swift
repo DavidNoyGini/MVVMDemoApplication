b27f24902f19a54051621a399c8b2cdddd5668aa
David Noy <davidn@gini-apps.com>
2021-11-03 15:03:22 +0200

    - Initial Commit

---
+++ MVVMDemoApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMDemoApplication
+//
+//  Created by David Noy on 03/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
